e9708ee59002c12de3a2e93581a17952f8fd87c2
jaqueetuzao <jaqueetuzao@gmail.com>
2025-10-29 10:29:25 -0300

    criação da página inicial

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include ('db_connection.php'); ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,10 +9,30 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Minha To_Do_List</h1>
+    <h1>Minha Lista de Tarefas</h1>
     <form action="add_task.php" method="POST">
         <input type="text" name="title" placeholder="Nova tarefa..." required>
         <button type="submit">Adicionar</button>
     </form>
+    <hr>
+    <ul>
+        <?php
+        $sql = "SELECT * FROM tasks ORDER BY id DESC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<li>";
+                echo $row['status'] == 'concluida' ? "<s>{$row['title']}</s>" : $row['title'];
+                echo "
+                        <a href='update_task.php?id={$row['id']}'>[X]</a>
+                        <a href='delete_task.php?id={$row['id']}'>[J]</a>";
+                        echo "</li>";
+            }
+        } else {
+            echo "<p>Nenhuma tarefa cadastrada.</p>";
+        }
+        ?>
+    </ul>
 </body>
 </html>
